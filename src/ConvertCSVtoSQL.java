05d2cccbb57d579b55dd506f2971ddb090fe94ce
Ernesto Rendon <egrendon.dev@gmail.com>
2017-02-15 16:42:46 -0700

    Update to input file

--- src/ConvertCSVtoSQL.java
+++ src/ConvertCSVtoSQL.java
@@ -14,7 +14,7 @@ class ConvertCSVtoSQL
     {
 
 //    	File inFile = getFileFromSrc("src/MobileContentAuditForDev_012617.csv");
-    	File inFile = getFileFromSrc("files/input/MobileContentAuditForDev_012617_3.csv");
+    	File inFile = getFileFromSrc("files/input/MobileContentAuditForDev_012617_V5.txt");
     	List<String[]> rows = readFileIO(inFile);
     	String updateScriptString = creatSQLFile(rows, 5);
     	String rollBackScriptString = creatSQLFile(rows, 4);
